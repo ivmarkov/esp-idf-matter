eb6ec2b4a2c7faeac42e4e961fbeec84b982aa55
ivmarkov <ivan.markov@gmail.com>
2024-06-01 10:10:17 +0000

    Bugfix: wrong index reported

--- src/wifi/comm.rs
+++ src/wifi/comm.rs
@@ -237,7 +237,7 @@ where
                         writer.set(NetworkConfigResponse {
                             status: NetworkCommissioningStatus::Success,
                             debug_text: None,
-                            network_index: Some(state.networks.len() as _),
+                            network_index: Some((state.networks.len() - 1) as _),
                         })?;
                     }
                     Err(network) => {
